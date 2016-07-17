0b6e75961f33a5451ea1c28fce94f462fb93107b
CodeBreakPP <tjudxy19930805@gmail.com>
2016-07-17 21:56:30 +0900

    Add files via upload

--- string_matching_finite_automata.py
+++ string_matching_finite_automata.py
@@ -1,18 +1,34 @@
-def trans(s):
-    m=len(s)
-    sigma=set(s)
-    print sigma
-    trans={}
-    for i in range(m+1):
-        trans[i]={}
-        for j in sigma:
-            trans[i][j]={}
-            k=min(m+1,i+2)
-            while k>=1:
-                k=k-1
-                if s[0:k]==(s[0:i]+j)[i+1-k:i+1]:
-                    break
-            trans[i][j]=k
-    return trans                
+
+class smia:
+
+    def __init__(self):
+        self.trans={}
+        self.length=0
+        
+    def setTrans(self,s):
+        m=len(s)
+        self.length=len(s)
+        sigma=set(s)
+        self.trans={}
+        for i in range(m+1):
+            self.trans[i]={}
+            for j in sigma:
+                self.trans[i][j]={}
+                k=min(m+1,i+2)
+                while k>=1:
+                    k=k-1
+                    if s[0:k]==(s[0:i]+j)[i+1-k:i+1]:
+                        break
+                self.trans[i][j]=k
+                
+    def match(self,s):
+        k=0
+        for i in range(len(s)):
+            c=s[i]
+            k=self.trans[k][c]
+            if k==self.length:
+                return i
+        return -1
+        
     
     
